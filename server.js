d691a0b149e9200db616e032a114bf229da02e5c
s1gosain <39508333+s1gosain@users.noreply.github.com>
2018-12-06 12:43:25 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const path = require('path');
-const request = require('request');
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-      res.send('hi this works');
-})
 app.post('/calculator', (req, res) => {
-    let rateCard = req.body.rateCard;
-    let remnantCard = req.body.remnantCard;
-    let production = req.body.production;
-    let install =  req.body.install;
+       if (!req.body.initial || !req.body.markup) return res.status(500).send('error');
+    else {
+    let initial = req.body.initial;
     let markup = req.body.markup;
-
-    // if (req.body)
-}, (err, results) => {
-  if (err) return res.status(500).send('error');
-  return res.status(200).json(results);
+    let newPrice = initial + (markup/100) * initial;
+    let responseData = {
+      price: newPrice,
+      higher: newPrice > initial
+    };
+    res.json(responseData)
 })
 
 
